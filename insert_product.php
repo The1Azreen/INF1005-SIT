829e612429bd61e3ffe6653054c6dfbc34e285c3
The1Azreen <azreen654@gmail.com>
2024-03-21 11:48:51 +0800

    Added preliminary files to test image insertion and display

--- insert_product.php
+++ insert_product.php
@@ -26,6 +26,15 @@
             $brandOptions .= '<option value="' . $row["brand_title"] . '">' . $row["brand_title"] . '</option>';
         }
     }
+       // Fetch category titles from the database
+       $categoryOptions = "";
+       $sql = "SELECT category_title FROM ShopCategoryPrototypeTable";
+       $result = $conn->query($sql);
+       if ($result->num_rows > 0) {
+           while ($row = $result->fetch_assoc()) {
+               $categoryOptions .= '<option value="' . $row["category_title"] . '">' . $row["category_title"] . '</option>';
+           }
+       }
     ?>
 </head>
 <body class="bg-light">
@@ -50,8 +59,40 @@
          <?php echo $brandOptions; ?>
         </select>
       </div>
+     <!-- Category -->
+    <div class="form-outline mb-4 w-50 m-auto">
+        <select name="product_category" id="product_category" class="form-select">
+          <option value="">Select a Category</option>
+          <?php echo $categoryOptions; ?>
+        </select>
+      </div>
+      <!-- keywords -->
+      <div class="form-outline mb-4 w-50 m-auto">
+        <label for="product_keywords" class="form-label">Product Keywords</label>
+        <input type="text" name="product_keywords" id="product_keywords" class="form-control" placeholder="Enter product keywords" autocomplete="off" required="required">
+      </div>
+      <!-- Image 1 -->
+      <div class="form-outline mb-4 w-50 m-auto">
+        <label for="product_image1" class="form-label">Product image 1</label>
+        <input type="file" name="product_image1" id="product_image1" class="form-control" required="required">
+      </div>
+      <!-- Image 2 -->
+      <div class="form-outline mb-4 w-50 m-auto">
+        <label for="product_image2" class="form-label">Product image 2</label>
+        <input type="file" name="product_image2" id="product_image2" class="form-control" required="required">
+      </div>
+       <!-- Image 3 -->
+       <div class="form-outline mb-4 w-50 m-auto">
+        <label for="product_image3" class="form-label">Product image 3</label>
+        <input type="file" name="product_image3" id="product_image3" class="form-control" required="required">
+      </div>
 
-      <button type="submit" class="btn btn-primary">Submit</button>
+
+      <!-- Submit -->
+      <div class="form-outline mb-4 w-50 m-auto text-center">
+        <button type="submit" name="insert_product" class="btn btn-info" value="Insert Products">Insert Products</button>
+      </div>
+
     </form>
   </div>
 </body>
