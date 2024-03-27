7a411659fbe24a2359520f7ade83d6fba77f69e9
The1Azreen <azreen654@gmail.com>
2024-03-27 15:39:28 +0800

    Test

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -1,51 +1,45 @@
-<nav class="navbar navbar-expand-sm sticky-top">
-    <a class="navbar-brand" href="/">
-        <img src="images/circuitcart-logo.png" alt="Logo" height="50em">
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="index.php">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">About Us</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Products</a>
-            </li>
-        </ul>
-        <!-- Icon -->
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li class="nav-item dropdown">
-                <a class="nav-link" href="/login.php" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <i class="material-icons-outlined">account_circle</i>
-                </a>
-                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="/register.php">Register</a></li>
-                    <li><a class="dropdown-item" href="/login.php">Login</a></li>
-                </ul>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/cart.php">
-                    <i class="material-icons-outlined">shopping_cart</i>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <i class="material-icons-outlined">search</i>
-                </a>
-            </li>
-
-        </ul>
-        <!--  <input type="checkbox" class="checkbox" id="chk">
-        <label class="label" for="chk">
-            <i class="material-icons-outlined">dark_mode</i>
-            <i class="material-icons-outlined">light_mode</i>
-
-            <div class="ball"></div>
-        </label> -->
+<nav class="navbar navbar-expand-lg navbar-expand-sm bg-body-tertiary sticky-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">
+            <img src="images/circuitcart-logo.png" alt="Logo" height="50em">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="aboutUs.php">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="product.php">Products</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <span class="material-icons-outlined">account_circle</span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="register.php">Register</a></li>
+                        <li><a class="dropdown-item" href="login.php">Login</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="cart.php">
+                        <span class="material-icons-outlined">shopping_cart</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <span class="material-icons-outlined">search</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
