0a61ceb1789405548aeda13b5d2281de5bed3fd8
The1Azreen <azreen654@gmail.com>
2024-03-27 19:06:30 +0800

    qe

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <nav class="navbar navbar-expand-lg navbar-expand-sm bg-body-tertiary sticky-top">
     <div class="container-fluid">
         <a class="navbar-brand" href="/">
@@ -41,5 +42,37 @@
                 </li>
             </ul>
         </div>
+=======
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">
+        <img src="images/logo.jpg" alt="Logo" height="50em">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#dogs">Dogs</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#cats">Cats</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="register.php">Register</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="login.php">Login</a>
+            </li>
+        </ul>
+>>>>>>> parent of 0037b96 (Merge branch 'Azreen-Branch' of https://github.com/The1Azreen/INF1005-SIT into Azreen-Branch)
     </div>
 </nav>
--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 </head>
 
 <body>
+<<<<<<< HEAD
 
     <!--     <?php
                 // Include header.inc.php for the header section
@@ -130,3 +131,102 @@
 </body>
 
 </html>
+=======
+
+<?php
+// Include header.inc.php for the header section
+include "inc/header.inc.php";
+?>
+
+<?php
+// Include nav.inc.php for the navigation menu
+include "inc/nav.inc.php";
+?>
+
+<main class="container">
+    <section id="dogs">
+        <h2 class="sectionheader">All About Dogs!</h2>
+        <div class="row">
+            <article class="col-sm">
+                <h3>Poodles</h3>
+                <figure>
+                    <img class="img-thumbnail" src="images/poodle_small.jpg"
+                         alt="Poodle" title="View larger image..."/>
+                    <figcaption>Standard Poodle</figcaption>
+                </figure>
+                <p>
+                    Poodles are a group of formal dog breeds, the Standard
+                    Poodle, Miniature Poodle and Toy Poodle. The height of the poodle is typically between 18 and 24 inches,
+                    although being over 15 inches specifically sets the Standard Poodle apart from Miniature Poodle and Toy Poodle.
+                </p>
+            </article>
+
+            <article class="col-sm">
+                <h3>Chihuahua</h3>
+                <figure>
+                    <img class="img-thumbnail" src="images/chihuahua_small.jpg"
+                         alt="Chihuahua" title="View larger image..."/>
+
+                    <figcaption>Chihuahua</figcaption>
+                </figure>
+                <p>
+                    The Chihuahua is the smallest breed of dog, and is named
+                    after the Mexican state of Chihuahua.There are two variables of Chihuahua - The Smooth Coat (smooth-haired)
+                    and the Long Coat(long-haried). Both the Smooth and Long Coat have their special attractions and are equally
+                    easy keep clean and well-groomed.
+                </p>
+            </article>
+        </div>
+    </section>
+
+    <section id="cats">
+        <h2 class="sectionheader">All About Cats!</h2>
+        <div class="row">
+            <article class="col-sm">
+                <h3>Tabby</h3>
+                <figure>
+                    <img class="img-thumbnail" src="images/tabby_small.jpg"
+                         alt="Tabby" title="View larger image..."/>
+                    <figcaption>Tabby Cat</figcaption>
+                </figure>
+                <p>
+                    “A tabby is any domestic cat (Felis catus) with a distinctive ‘M’ shaped marking on its forehead,
+                    stripes by its eyes and across its cheeks, along its back, and around its legs and tail, and (differing by tabby type),
+                    characteristic striped, dotted, lined, flecked, banded or swirled patterns on the body-neck, shoulders, sides,
+                    flanks, chest and abdomen. ‘Tabby’ is not a breed of cat but a coat type seen in almost all genetic lines of domestic cats,
+                    regardless of breed or pedigree status.
+                </p>
+            </article>
+
+            <article class="col-sm">
+                <h3>Calico</h3>
+                <figure>
+                    <img class="img-thumbnail" src="images/calico_small.jpg"
+                         alt="Calico" title="View larger image..."/>
+                    <figcaption>Calico Cat</figcaption>
+                </figure>
+                <p>
+                    A calico cat is a domestic cat of any breed with a tri-color coat. The calico cat on its forehead,
+                    stripes by its eyes and across its cheeks, along its back, and around its legs and tail, and
+                    (differing by tabby type), characteristic striped, dotted, lined, flecked, banded or
+                    swirled patterns on the body-neck, shoulders, sides, flanks, chest and abdomen.
+
+                    The most commonly thought of as being typically 25% to 75% white with large orange and black patches
+                    (or sometimes cream and grey patches); however, the calico cat can have any three colors in its pattern.
+                    They are almost exclusively female except under rare genetic conditions. Calico is not to be confused with a
+                    tortoiseshell, which has a mostly mottled coat of black/orange or grey/cream with relatively few to no
+                    white markings.
+                </p>
+            </article>
+        </div>
+    </section>
+</main>
+
+<?php
+// Include footer.inc.php for the footer section
+include "inc/footer.inc.php";
+?>
+
+</body>
+</html>
+>>>>>>> parent of 0037b96 (Merge branch 'Azreen-Branch' of https://github.com/The1Azreen/INF1005-SIT into Azreen-Branch)
