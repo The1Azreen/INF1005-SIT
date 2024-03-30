b215f1533282764e3ad42cb0166aaaa0ba7a2f95
The1Azreen <azreen654@gmail.com>
2024-03-30 21:19:37 +0800

    Update list_users.php

--- adminpages/list_users.php
+++ adminpages/list_users.php
@@ -2,20 +2,18 @@
 <table class="table table-bordered mt-5">
     <thead class="bg-info">
         <tr class="text-center">
-            <th>Serial number</th>
+            <th>ID</th>
             <th>Username</th>
-            <th>User email</th>
-            <th>User address</th>
-            <th>User mobile</th>
+            <th>Email</th>
+            <th>Address</th>
             <th>Delete</th>
         </tr>
     </thead>
     <tbody class='bg-secondary text-light'>
         <?php
-        $get_payments = "Select * from `user_table`";
+        $get_payments = "SELECT * FROM `user_table`";
         $result = mysqli_query($con, $get_payments);
         $row_count = mysqli_num_rows($result);
-
 
         if ($row_count == 0) {
             echo "<h2 class='text-danger text-center mt-5'>No users yet</h2>";
@@ -26,20 +24,16 @@
                 $username = $row_data['username'];
                 $user_email = $row_data['user_email'];
                 $user_address = $row_data['user_address'];
-                $user_mobile = $row_data['user_mobile'];
                 $number++;
                 echo " <tr>
                 <td>$number</td>
                 <td>$username</td>
                 <td>$user_email</td>
                 <td>$user_address</td>
-                <td>$user_mobile</td>
                 <td><a href='' class='text-light'><i class='fa-solid fa-trash'></i></a></td>
                 </tr>";
             }
         }
-
         ?>
-
     </tbody>
 </table>
