f9dfc79e842ce3fd7c41648e3e6b23679cdff77f
The1Azreen <azreen654@gmail.com>
2024-04-02 14:48:04 +0800

    Added jocelyn php

--- index.php
+++ index.php
@@ -4,15 +4,17 @@ session_start(); // Start the session
 <!DOCTYPE html>
 <html lang="en">
 
+
 <head>
     <?php
     include "inc/head.inc.php";
     ?>
 </head>
 
+
 <body>
     <?php // Check if user is login or not
-    if (isset ($_SESSION["user"]) == "") {
+    if (isset($_SESSION["user"]) == "") {
         include "inc/nav.inc.php";
     } else {
         include "inc/loginNav.inc.php";
@@ -31,20 +33,17 @@ session_start(); // Start the session
 
                     <!--EACH CAROUSELL CARD-->
                     <div class="carousel-item active">
-                        <img class="d-block w-100" src="images/Banners/hugo-agut-tugal-6cdIdu8KkLg-unsplash.jpg"
-                            alt="First slide">
+                        <img class="d-block w-100" src="images/Banners/hugo-agut-tugal-6cdIdu8KkLg-unsplash.jpg" alt="First slide">
                         <div class="carousel-caption d-none d-md-block text-center">
                             <h5>BUY NOW</h>
                         </div>
                     </div>
 
                     <div class="carousel-item">
-                        <img class="d-block w-100"
-                            src="images/Banners/linkedin-sales-solutions-YDVdprpgHv4-unsplash.jpg" alt="Second slide">
+                        <img class="d-block w-100" src="images/Banners/linkedin-sales-solutions-YDVdprpgHv4-unsplash.jpg" alt="Second slide">
                     </div>
                     <div class="carousel-item">
-                        <img class="d-block w-100" src="images/Banners/nsys-group-ZvhZBzwmLic-unsplash.jpg"
-                            alt="Third slide">
+                        <img class="d-block w-100" src="images/Banners/nsys-group-ZvhZBzwmLic-unsplash.jpg" alt="Third slide">
                     </div>
                 </div>
 
@@ -61,94 +60,87 @@ session_start(); // Start the session
             </div>
         </section>
 
-        <!--FLASH DEALS-->
         <section id="flash_deals">
-            <div class="container-fluid bg-3 text-center">
-                <br>
-                <h3 class="text-center">FLASH DEALS</h3>
-                <br>
-                <div id="flashDealsCarousel" class="carousel slide" data-ride="carousel">
-                    <!-- Indicators -->
-                    <ol class="carousel-indicators">
-                        <li data-target="#flashDealsCarousel" data-slide-to="0" class="active"></li>
-                        <li data-target="#flashDealsCarousel" data-slide-to="1"></li>
-                        <li data-target="#flashDealsCarousel" data-slide-to="2"></li>
-                    </ol>
+            <div class="flash-deals-container">
+                <div class="container-fluid bg-3 text-center">
+                    <h3 class="text-center">FLASH DEALS</h3>
+                    <div id="flashDealsCarousel" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            <?php
+                            $flashDeals = [
+                                ["images/flash_deals/black_digital_camera.jpeg?product_id=1001", "Black Digital Camera"],
+                                ["images/flash_deals/silver_laptop.jpeg?product_id=1002", "Silver Laptop"],
+                                ["images/flash_deals/fitness_tracker.jpeg", "Fitness Tracker"],
+                                ["images/flash_deals/smart_speaker.jpeg", "Smart Speaker"],
+                                ["images/flash_deals/wireless_headset.jpeg", "Wireless Headset"]
+                            ];
 
-                    <!-- Wrapper for slides -->
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <div class="row">
-                                <div class="col-sm-3">
-                                    <img src="images/flash_deals/black_digital_camera.jpeg" class="img-responsive"
-                                        style="width:70%" alt="Image">
-                                </div>
-                                <div class="col-sm-3">
-                                    <img src="images/flash_deals/black_digital_camera.jpeg" class="img-responsive"
-                                        style="width:70%" alt="Image">
-                                </div>
-                                <div class="col-sm-3">
-                                    <img src="images/flash_deals/black_digital_camera.jpeg" class="img-responsive"
-                                        style="width:70%" alt="Image">
-                                </div>
-                                <div class="col-sm-3">
-                                    <img src="images/flash_deals/black_digital_camera.jpeg" class="img-responsive"
-                                        style="width:70%" alt="Image">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="row">
-                                <div class="col-sm-3">
-                                    <img src="images/flash_deals/silver_laptop.jpeg" class="img-responsive"
-                                        style="width:70%" alt="Image">
-                                </div>
-                                <div class="col-sm-3">
-                                    <img src="images/flash_deals/silver_laptop.jpeg" class="img-responsive"
-                                        style="width:70%" alt="Image">
-                                </div>
-                                <div class="col-sm-3">
-                                    <img src="images/flash_deals/silver_laptop.jpeg" class="img-responsive"
-                                        style="width:70%" alt="Image">
-                                </div>
-                                <div class="col-sm-3">
-                                    <img src="images/flash_deals/silver_laptop.jpeg" class="img-responsive"
-                                        style="width:70%" alt="Image">
-                                </div>
-                            </div>
-                        </div>
-                        <!-- <div class="row">
 
-                            <div class="col-sm-4">
-                                <img src="images/flash_deals/black_digital_camera.jpeg" class="img-responsive" style="width:70%" alt="Image">
-                            </div>
-                            <div class="col-sm-4">
-                                <img src="images/flash_deals/black_digital_camera.jpeg" class="img-responsive" style="width:70%" alt="Image">
-                            </div>
-                            <div class="col-sm-4">
-                                <img src="images/flash_deals/black_digital_camera.jpeg" class="img-responsive" style="width:70%" alt="Image">
-                            </div>
-                            <div class="col-sm-4">
-                                <img src="images/flash_deals/black_digital_camera.jpeg" class="img-responsive" style="width:70%" alt="Image">
-                            </div>
-                        </div> -->
+
+                            $numSlides = ceil(count($flashDeals) / 4); // Calculate the number of slides
+
+                            for ($i = 0; $i < $numSlides; $i++) {
+                                $activeClass = ($i === 0) ? "active" : "";
+                            ?>
+                                <li data-target="#flashDealsCarousel" data-slide-to="<?php echo $i; ?>" class="<?php echo $activeClass; ?>"></li>
+                            <?php } ?>
+                        </ol>
+                        <div class="carousel-inner">
+                            <?php for ($i = 0; $i < $numSlides; $i++) {
+                                $activeClass = ($i === 0) ? "active" : "";
+                            ?>
+                                <div class="carousel-item <?php echo $activeClass; ?>">
+                                    <div class="row">
+                                        <!-- <?php for ($j = $i * 4; $j < min(($i + 1) * 4, count($flashDeals)); $j++) {
+                                                    $deal = $flashDeals[$j];
+                                                ?>
+                                            <div class="col-sm-3">
+                                                <a href="product_description.php?">
+                                                    <img src="<?php echo $deal[0]; ?>" class="img-responsive product-image" style="width:70%" alt="<?php echo $deal[1]; ?>">
+                                                </a>
+                                            </div>
+                                        <?php } ?> -->
+                                        <?php for ($j = $i * 4; $j < min(($i + 1) * 4, count($flashDeals)); $j++) {
+                                            $deal = $flashDeals[$j];
+                                            $url = $deal[0];
+                                            $query = parse_url($url, PHP_URL_QUERY);
+                                            parse_str($query, $params);
+                                            $productId = $params['product_id'];
+                                        ?>
+                                            <div class="col-sm-3">
+                                                <a href="product_description.php?product_id=<?php echo $productId; ?>">
+                                                    <img src="<?php echo $deal[0]; ?>" class="img-responsive product-image" style="width:70%" alt="<?php echo $deal[1]; ?>">
+                                                </a>
+                                            </div>
+                                        <?php } ?>
+                                    </div>
+                                </div>
+                            <?php } ?>
+                        </div>
+                        <div class="carousel-control">
+                            <a class="carousel-control-prev" href="#flashDealsCarousel" role="button" data-slide="prev" style="width: 5%;">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#flashDealsCarousel" role="button" data-slide="next" style="width: 5%;">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+
                     </div>
-                    <!--ARROWS-->
-                    <a class="carousel-control-prev" href="#flashDealsCarousel" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#flashDealsCarousel" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
+                </div>
+            </div>
         </section>
-        <!--HOTTEST SELLING SECTION-->
+
     </main>
-    <br><br>
+
+
     <?php
-        include "inc/footer.inc.php";
+    // Include footer.inc.php for the footer section
+    include "inc/footer.inc.php";
     ?>
+
 </body>
 
 </html>
---
+++ product_description.php
@@ -0,0 +1,124 @@
+<!-- <?php
+      ini_set('display_errors', 1);
+      ini_set('display_startup_errors', 1);
+      error_reporting(E_ALL);
+      ?>
+<?php
+var_dump($_GET);
+?> -->
+
+<?php
+session_start(); // Start the session
+$productId = isset($_GET['product_id']) ? $_GET['product_id'] : null; // Get the product id from the URL
+if ($productId !== null) {
+  // getProduct($productId); // Call the function with the product id
+  $productId = intval($_GET['product_id']);
+  if ($productId <= 0) {
+    // Handle the case when product_id is not a valid integer
+    echo "Invalid product ID.";
+  }
+} else {
+  echo "Product ID is not set.";
+  exit;
+}
+function getProduct($productId)
+{
+  global $productId, $productName, $price, $desc, $image_path, $errorMsg, $success;
+
+  // Create database connection.
+  $config = parse_ini_file('/var/www/private/db-config.ini');
+  if (!$config) {
+    $errorMsg = "Failed to read database config file.";
+    $success = false;
+  } else {
+    $conn = new mysqli(
+      $config['servername'],
+      $config['username'],
+      $config['password'],
+      $config['dbname']
+    );
+    // Check connection
+    if ($conn->connect_error) {
+      $errorMsg = "Connection failed: " . $conn->connect_error;
+      $success = false;
+    } else {
+      $stmt = $conn->prepare("SELECT * FROM products WHERE product_id = ?");
+      if (!$stmt) {
+        throw new Exception("Failed to prepare statement: " . $conn->error);
+      }
+      $stmt->bind_param("i", $productId);
+      if (!$stmt->bind_param("i", $productId)) {
+        throw new Exception("Failed to bind parameters: " . $stmt->error);
+      }
+      $stmt->execute();
+      if (!$stmt->execute()) {
+        throw new Exception("Failed to execute statement: " . $stmt->error);
+      }
+      $result = $stmt->get_result();
+      if ($result->num_rows > 0) {
+        $product = $result->fetch_assoc();
+        $productId = $product['product_id'];
+        $productName = $product['product_name'];
+        $price = $product['price'];
+        $desc = $product['product_description'];
+        $image_path = $product['filePath'];
+      } else {
+        echo 'This product is not available.';
+      }
+      $stmt->close();
+      $conn->close();
+    }
+  }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php
+  // Call the getProduct function to retrieve the product details
+  getProduct($productId);
+  ?>
+  
+  <title><?php echo $productName; ?> | CircuitCart</title>
+  <?php
+  // Include head.inc.php for the <head> section
+  include "inc/head.inc.php";
+  ?>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.*/dist/css/bootstrap.min.css">
+  <style>
+    /* Add custom styles here if needed */
+  </style>
+</head>
+
+<body>
+  <!-- Navigation menu inclusion -->
+  <?php include "inc/nav.inc.php"; ?>
+
+  <!-- Main Content -->
+  <div class="container mt-5">
+    <div class="row">
+      <!-- Product Description Column -->
+      <div class="col-md-6">
+        <h2 class="product-title"><?php echo $productName; ?></h2>
+        <p><?php echo $desc; ?></p>
+        <h3 class="price">$<?php echo $price; ?></h3>
+        <div class="alert alert-warning" role="alert">
+          Note: A cancellation fee will apply. Cancellation fees may differ depending on your region.
+        </div>
+        <a href="/cart.php" class="btn btn-primary" role="button">Add to Cart</a>
+      </div>
+
+      <!-- Product Image Column -->
+      <div class="col-md-6">
+        <img src="<?php echo $image_path ?>" alt="<?php echo $productName ?>" class="img-fluid">
+      </div>
+    </div>
+  </div>
+
+  <!-- Footer section inclusion -->
+  <?php include "inc/footer.inc.php"; ?>
+
+</body>
+
+</html>
